9865275bcba00c6e95e0977b4566b568e19289d2
Gagandeep Singh <Gagan@Gagandeeps-MacBook-Pro.local>
2018-08-01 13:18:50 -0700

    path module

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //use const to import modules as we wont be able to change the value for that
 const logger = require('./logger');
+const path = require('path');
 
 global.console.log(logger);
 
 console.log(__dirname);
 console.log(__filename);
+
+var pathp = path.parse(__filename);
+
+console.log(pathp);
 
 //logger = 1;
 
