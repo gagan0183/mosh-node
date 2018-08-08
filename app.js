974e67ba798406bd7934fc89807aad3fb9b28ce3
Gagandeep Singh <Gagan@Gagandeeps-MacBook-Pro.local>
2018-08-08 12:30:19 -0700

    That is request response for createserver method of http module

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer(function(req, res) {
+    if(req.url == '/') {
+        res.write('This is the home page app');
+        res.end();
+    }
+});
 
 server.on('connection', (sot) => {
     console.log('connect');
